439e77360c8e39a81cf6e2ad303d958e1a3d643b
SirPopGhost <srikardevesetti@gmail.com>
2024-02-05 16:15:07 -0800

    Added two x on credit for readme

--- calc.py
+++ calc.py
@@ -1,1 +1,30 @@
+print("Welcome to the calculator!")
+num1 = int(input("Please enter your first number: "))
+operator = input("Enter the operator here. ")
+num2 = int(input("Please enter your second number: "))
+
+if operator == '+':
+	print(f"{num1} + {num2} = {num1 + num2}")
+elif operator == '-':
+	print(f"{num1} - {num2} = {num1 - num2}")
+elif operator == "*":
+	print(f"{num1} * {num2} = {num1 * num2}")
+elif operator == "/":
+	if num2 == 0:
+		print("Error! Cannot divide number by 0!")
+	else:
+		print(f"{num1} / {num2} = {num1 / num2}")
+elif operator == "**":
+	print(f"{num1} ** {num2} = {num1 ** num2}")
+elif operator == "//":
+	print(f"{num1} // {num2} = {num1 // num2}")
+elif operator == "%":
+	print(f"{num1} % {num2} = {num1 % num2}")
+else:
+	print(f"{operator} is not a valid operator!")
+
+
+
+
+
 
